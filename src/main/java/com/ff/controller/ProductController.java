9916593d053a9225e7ca9be4f9715a307cb79084
truongnguyen-kt <truongnhlse160191@fpt.edu.vn>
2023-04-09 15:43:52 +0700

    manage controller

--- src/main/java/com/ff/controller/ProductController.java
+++ src/main/java/com/ff/controller/ProductController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/product")
+@RestController
+@RequestMapping("/product")
 public class ProductController {
     @Autowired
     ProductService productService;
